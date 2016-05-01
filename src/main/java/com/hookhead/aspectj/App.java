2c3c71dd11e238d77480283d7a60ae6414b42c32
Mark Burke <Mark.Burke@hookhead.com>
2016-05-01 13:14:58 +0100

    Initial implementation of MethodLogger aspect

--- src/main/java/com/hookhead/aspectj/App.java
+++ src/main/java/com/hookhead/aspectj/App.java
@@ -1,26 +1,43 @@
 package com.hookhead.aspectj;
 
+import com.apm4all.tracy.Tracy;
+import com.apm4all.tracy.extensions.annotations.Profiled;
+import com.apm4all.tracy.extensions.annotations.Qualify;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
+    private static final String TASK_ID_VALUE = "taskId";
+    private static final String PARENT_OPT_ID_VALUE = "parentOptId";
+    private static final String COMPONENT_VALUE = "component";
+
     public static void main(String[] args) {
-        LOGGER.info("Main");
+
+        Tracy.setContext(TASK_ID_VALUE, PARENT_OPT_ID_VALUE, COMPONENT_VALUE);
 
         outer("main1", "main2", "main3");
+
+        List<String> events = Tracy.getEventsAsJson();
+        for (String event : events) {
+            LOGGER.info(event);
+        }
+
+        Tracy.clearContext();
     }
 
-    @TracyLogger
+    @Profiled(annotations = {"param1", "param2", "param3"} ,name = "outer", qualify = Qualify.PACKAGE)
     protected static String outer(String param1, String param2, String param3) {
         inner("outer1");
 
         return "outer";
     }
 
-    @TracyLogger
+    @Profiled(annotations = "..")
     private static String inner(String param) {
         return "inner";
     }
--- src/main/java/com/hookhead/aspectj/MethodLogger.java
+++ src/main/java/com/hookhead/aspectj/MethodLogger.java
@@ -1,5 +1,8 @@
 package com.hookhead.aspectj;
 
+import com.apm4all.tracy.Tracy;
+import com.apm4all.tracy.extensions.annotations.Constants;
+import com.apm4all.tracy.extensions.annotations.Profiled;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,38 +12,112 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
-
 @Aspect
 public class MethodLogger {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodLogger.class);
 
-    @Around("execution(* *(..)) && @annotation(TracyLogger)")
+    @Around("execution(* *(..)) && @annotation(Profiled)")
     public Object around(ProceedingJoinPoint point) throws Throwable {
 
+        //
+        // Get method name
+        //
+
         MethodSignature methodSignature = MethodSignature.class.cast(point.getSignature());
-        String methodName = methodSignature.getName();
+        Profiled profiled = methodSignature.getMethod().getAnnotation(Profiled.class);
 
-        LOGGER.info("Tracing method " + methodName);
+        StringBuilder methodNameBuilder = new StringBuilder();
+        switch(profiled.qualify()) {
+            case NO:
+                break;
+            case CLASS:
+                methodNameBuilder.append(methodSignature.getDeclaringType().getSimpleName());
+                methodNameBuilder.append(".");
+                break;
+            case PACKAGE:
+                methodNameBuilder.append(methodSignature.getDeclaringType().getCanonicalName());
+                methodNameBuilder.append(".");
+                break;
+        }
 
-        TracyLogger tracyLogger = methodSignature.getMethod().getAnnotation(TracyLogger.class);
-        List<String> traceParameters = new ArrayList<>(Arrays.asList(tracyLogger.params()));
+        String methodName = profiled.name();
+        if (methodName.equals("")) {
+            methodName = methodSignature.getName();
+        }
+        methodNameBuilder.append(methodName);
+        String qualifiedMethodName = methodNameBuilder.toString();
+
+        LOGGER.info("Qualified Method Name = " + qualifiedMethodName);
+
+        //
+        // Get the annotations
+        //
 
         boolean traceAll = false;
-        String[] parameterNames = tracyLogger.params();
-        if (parameterNames.length == 1 && parameterNames[0].equals("..")) {
+        String[] parameterNames = profiled.annotations();
+        if (parameterNames.length == 1 && parameterNames[0].equals(Constants.ALL_PARAMETERS)) {
             traceAll = true;
         }
 
+        LOGGER.info("TraceAll = " + traceAll);
+
         int count = 0;
+        List<String> annotations = new ArrayList<>(Arrays.asList(profiled.annotations()));
+
+        //
+        // Tracy.before
+        //
+
+        Tracy.before(qualifiedMethodName);
+
+        //
+        // Tracy.annotate
+        //
         for (String paramName : methodSignature.getParameterNames()) {
-            if (traceAll || traceParameters.contains(paramName)) {
+            if (traceAll || annotations.contains(paramName)) {
                 Object argValue = point.getArgs()[count];
+                Tracy.annotate(paramName, argValue.toString());
                 LOGGER.info("\tParameter:" + paramName + "=" + argValue);
             }
             count++;
         }
 
-        Object result = point.proceed();
+        Object result;
+        try {
+            result = point.proceed();
+        } catch(Throwable t) {
+            List<Class> includeExceptions = new ArrayList<>(Arrays.asList(profiled.include()));
+            if (includeExceptions.size() == 0 || includeExceptions.contains(t.getClass())) {
+                List<Class> excludeExceptions = new ArrayList<>(Arrays.asList(profiled.exclude()));
+                if (!excludeExceptions.contains(t.getClass())) {
+                    switch (profiled.onException()) {
+                        case DO_NOTHING:
+                            break;
+                        case POP:
+                            // FIXME
+                            break;
+                        case MARK_FRAME:
+                            // FIXME
+                            break;
+                        case MARK_UP_TO_THE_ROOT_FRAME:
+                            // FIXME
+                            break;
+                        default:
+                            LOGGER.error("Unhandled case in switch " + profiled.onException());
+                            break;
+                    }
+                }
+            }
+
+            throw t;
+        }
+
+        if (profiled.captureOutput()) {
+            LOGGER.info("Adding '" + Constants.METHOD_RESULT + "' attribute " + result);
+            Tracy.annotate(Constants.METHOD_RESULT, result.toString());
+        }
+
+        Tracy.after(qualifiedMethodName);
 
         LOGGER.info("\tReturn:" + result);
 
